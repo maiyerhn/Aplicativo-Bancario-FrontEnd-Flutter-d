dba406f256d78dd89d0100be975e8b083e0dc71b
maiyerhn <118492521+maiyerhn@users.noreply.github.com>
2024-06-15 12:24:43 -0500

    Clase Datos
    La usare para darle persistencia a datos como el dinero y no se pierda esa información cuando se  cierre la aplicación

---
+++ bankco/lib/models/datos.dart
@@ -0,0 +1,6 @@
+class Datos {
+  int dinero = 0;
+  int prestamo = 0;
+  int meses = 0;
+  
+}
--- bankco/lib/models/user.dart
+++ bankco/lib/models/user.dart
@@ -1,18 +1,22 @@
 import 'dart:convert';
 
+import 'package:bankco/models/datos.dart';
+
 class User{
   String? nombre;
   String? apellido;
   String? usuario;
   int? contrasena;
   String? tipo;
+  Datos datos;
 
   User(String jsonString){
+    datos = new Datos();
     Map data =jsonDecode(jsonString);
     this.nombre = data['nombre'];
     this.apellido = data['apellido'];
     this.usuario = data['usuario'];
     this.contrasena = data['contrasena'];
-    this.tipo = data['tipo'];
+    this.tipo = "usuario";
   }
 }
